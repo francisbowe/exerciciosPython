17cc6b388297ba5af98dd1a99de836835f96efdd
bowe11 <franciscodomingos20155@gmail.com>
2024-09-06 09:43:26 +0100

    correção da chamada da função gravar arquivo

--- exBibliotecsExeternasSO/converterGeolocalizacao.py
+++ exBibliotecsExeternasSO/converterGeolocalizacao.py
@@ -1,15 +1,15 @@
-
 from geopy.geocoders import Nominatim
 from fucoes.funcoes_Json import ler_arquivo, gravar_arquivo
 
 geolocator = Nominatim(user_agent="wazeyes")
-dicionario=ler_arquivo("entrada.json")
-lista=dicionario["endereco"]
-endereco=lista[0] + "," + lista[1] + " " + lista[2] + " " + lista[3]
+dicionario = ler_arquivo("entrada.json")
+lista = dicionario["endereco"]
+endereco = lista[0] + ", " + lista[1] + " " + lista[2] + " " + lista[3]
 location = geolocator.geocode(endereco)
+
 if location:
     saida = {"coordenadas": (location.latitude, location.longitude)}
-    gravar_arquivo(saida, "saida.json")
-    print("Coordenadas gravadas com sucesso.")
+    gravar_arquivo("saida.json", saida)  # Chamada corrigida
+    print(f"Coordenadas gravadas com sucesso. \n {endereco}")
 else:
     print(f"Não foi possível encontrar o endereço: {endereco}")
