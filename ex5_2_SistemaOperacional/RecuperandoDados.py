e65960146f3cbce31a8ce25af4ae9a65b3fc41a3
francis_bowe <franciscodomingos20155@gmail.com>
2024-11-08 13:14:59 +0100

    conversando com S.O via python

---
+++ ex5_2_SistemaOperacional/RecuperandoDados.py
@@ -0,0 +1,10 @@
+import platform
+
+print ("Distribuição do S.O..: ", platform.platform())
+print("Nome do S.O...........: ", platform.system())
+print("Versão do S.O..........: ", platform.release())
+print("Versão do Python.......: ", platform.python_version())
+print("Arquitetura do S.O....: ", platform.architecture())
+print("Processador............: ", platform.processor())
+print("Nome do Computador..........: ", platform.node())
+print("Tipo de maquina..........: ", platform.node())
