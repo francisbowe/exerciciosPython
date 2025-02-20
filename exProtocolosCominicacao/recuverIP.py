93697bd539a33030ae8978d8e1c87f3c5aefd1f0
Bowe <franciscodomingos20155@gmail.com>
2025-02-20 19:35:27 +0100

    recuperando os ips das urls

--- exProtocolosCominicacao/recuverIP.py
+++ exProtocolosCominicacao/recuverIP.py
@@ -1,1 +1,7 @@
 import socket
+resp = "S"
+while (resp=="S"):
+    url = input ("Digite URL: ")
+    ip =socket.gethostbyname(url) # Função que recupera o IP do site
+    print (f"O IP do site {url} é: {ip}")
+    resp = input ("Deseja continuar? [s/n]").upper() 
