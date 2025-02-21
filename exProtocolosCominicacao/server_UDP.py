ecfb5ca2e13e3a59eccd937f73014e188ed6b5ed
Bowe <franciscodomingos20155@gmail.com>
2025-02-21 19:38:06 +0100

    adiciona tratamento de exceção para fechamento seguro do socket no servidor UDP

--- exProtocolosCominicacao/server_UDP.py
+++ exProtocolosCominicacao/server_UDP.py
@@ -1,17 +1,19 @@
 from socket import *
 
-server = "localhost"
+server = "127.0.0.1"
 port = 43210
 
 obj_socket = socket(AF_INET, SOCK_DGRAM) #criando um socket com protocolo UDP
 obj_socket.bind((server, port)) #vinculando o socket ao endereco e porta
 print("Server pronto....")
-while True:
-    dados, origem = obj_socket.recvfrom(65535) #recebendo os dados 65535 limite de bytes
-    print("Origem..........:", origem)
-    print("Dados recebidos.:", dados.decode()) #decode exibe os dados em bytes no formato string
-    resposta = input("Digite a resposta: ")
-    obj_socket.sendto(resposta.encode(), origem) #enviando a resposta, encode convert string to bytes
+try:
+    while True:
+        dados, origem = obj_socket.recvfrom(65535) #recebendo os dados 65535 limite de bytes
+        print("Origem..........:", origem)
+        print("Dados recebidos.:", dados.decode()) #decode exibe os dados em bytes no formato string
+        resposta = input("Digite a resposta: ")
+        obj_socket.sendto(resposta.encode(), origem) #enviando a resposta, encode convert string to bytes
+finally:
     obj_socket.close() #fechando o socket
 
 # The server code is similar to the client code, but it uses the bind() method to bind the socket to an address and port.
