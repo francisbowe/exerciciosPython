656a021995fd6d10d22e5b10f193613d16586302
Bowe <franciscodomingos20155@gmail.com>
2025-02-21 23:21:38 +0100

    altera servidor FTP para 'ftp.ibge.gov.br' e muda diretório de trabalho para 'Censos'

--- exProtocolosCominicacao/protocol_FTP.py
+++ exProtocolosCominicacao/protocol_FTP.py
@@ -1,13 +1,15 @@
 from ftplib import *
 import getpass
 ftp_activo = False #a varivael esta com valor False pois representa conexao passiva
-ftp = FTP('ftp.ibiblio.org') #conectando ao servidor FTP
+ftp = FTP('ftp.ibge.gov.br') #conectando ao servidor FTP
 print(ftp.getwelcome()) #metodo welcome exibe a mensagem de boas vindas
 user = input("Username: ")
 password = getpass.getpass ("Password: ")
 ftp.login(user, password) #metodo login para autenticacao
 print("Diretorio atual de trabalho: ", ftp.pwd()) #metodo pwd exibe o diretorio atual
-ftp.cwd('pub') #metodo cwd muda o diretorio de trabalho
+ftp.cwd('Censos') #metodo cwd muda o diretorio de trabalho
+#ftp.cwd('Censo_Demografico_2022')
 print("Diretorio corrente: ", ftp.pwd())
+print(ftp.retrlines('LIST'))
 ftp.quit()
 
