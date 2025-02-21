fcdca66f2fe6eda509f927ef9989cc679b03b031
Bowe <franciscodomingos20155@gmail.com>
2025-02-21 12:07:54 +0100

    script cliente feiito

---
+++ exProtocolosCominicacao/client.py
@@ -0,0 +1,12 @@
+from socket import *
+
+server = "127.0.0.1"
+port = 43210
+msg = bytes(input("Digite algo: "), 'utf-8') #transformando a mensagem em bytes
+obj_socket = socket(AF_INET, SOCK_STREAM) #criando um socket
+obj_socket.connect((server, port)) #conectando o socket ao endereco e porta
+obj_socket.send(msg) #enviando a mensagem
+msg_resposta = obj_socket.recv(1024) #recebendo a mensagem
+print("Recebemos: ", msg_resposta)
+obj_socket.close() #fechando o socket
+# The client code is similar to the server code, but it uses the connect() method to connect to the server.
