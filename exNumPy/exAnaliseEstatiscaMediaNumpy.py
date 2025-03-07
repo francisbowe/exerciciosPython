1dd3f54e343d02875d8e0294a9e8deed1bd9add6
Bowe <franciscodomingos20155@gmail.com>
2025-03-07 21:47:45 +0100

    adiciona script para análise estatística básica com NumPy

---
+++ exNumPy/exAnaliseEstatiscaMediaNumpy.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+#criando um array 
+arr1 = np.array([10, 21, 32, 43, 48, 15, 76, 13])
+#Media
+print(np.mean(arr1))
+#Mediana
+print(np.median(arr1))
+#Desvio padrao
+print(np.std(arr1))
+#Variancia
+print(np.var(arr1))
+#Soma
+print(np.sum(arr1))
+#Produto
+print(np.prod(arr1))
+#Maximo
+print(np.max(arr1))
+#Minimo
+print(np.min(arr1))
+#Indice do maximo
+print(np.argmax(arr1))
+#Indice do minimo
+print(np.argmin(arr1))
